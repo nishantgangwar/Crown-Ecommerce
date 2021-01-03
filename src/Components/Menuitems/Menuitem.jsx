897d1f72030b24ed857b3f23f878e01c116be9bf
nishantgangwar <Nishantgangwar69@gmail.com>
2021-01-03 12:30:54 +0530

    kghghk

---
+++ src/Components/Menuitems/Menuitem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./menuitem.scss";
+const Menuitem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="content">
+        <div className="title">{title.toUpperCase()}</div>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default Menuitem;
---
+++ src/Components/Pages/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Homepage.scss";
+import Directory from "../../directory/directory.jsx";
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
